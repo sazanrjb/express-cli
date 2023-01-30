cdc3f24abed69b22b63d5381e1a902ec1a402803
Sajan Rajbhandari <sazanrjb@gmail.com>
2023-01-30 22:27:07 +0545

    Setup typeorm

--- src/commands/DummyCommand.ts
+++ src/commands/DummyCommand.ts
@@ -1,10 +1,21 @@
 import { CommandInterface } from 'core/interfaces/Command.interface';
+import { DB } from 'database/data-source';
 
 export class DummyCommand implements CommandInterface{
 
   static description: string = 'Dummy command';
 
-  execute(): any {
-    return 'Hi';
+  async execute(): Promise<any> {
+    DB.initialize()
+      .then(async () => {
+        const users = await DB
+          .createQueryBuilder()
+          .select()
+          .from('database', null)
+          .getRawMany();
+
+        console.log(users)
+      })
+      .catch((error) => console.log(error))
   }
 }
---
+++ src/database/data-source.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+require('dotenv').config();
+
+export const DB = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: true,
+  logging: true,
+  entities: [],
+  subscribers: [],
+  migrations: [],
+});
+
